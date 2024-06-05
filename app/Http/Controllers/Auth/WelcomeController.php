769ffac8bdaaeaf6e5dfb0def33044f642ed391b
Mikołaj Wojciechowski <wwojciechowskimikolaj@gmail.com>
2024-06-05 19:24:47 +0200

    filtrowanie produktow po kategorii i cenie

--- app/Http/Controllers/Auth/WelcomeController.php
+++ app/Http/Controllers/Auth/WelcomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\ProductCategory;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
@@ -11,11 +12,30 @@ class WelcomeController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
+        $filters = $request->query('filter');
+        $query = Product::query();
+        if(!is_null($filters)) {
+            if(array_key_exists('categories', $filters)){
+                $query = $query->whereIn('category_id', $filters['categories']);
+            }
+            if(!is_null($filters['price_min'])) {
+                $query = $query->where('price', '>=', $filters['price_min']);
+            }
+            if(!is_null($filters['price_min'])) {
+                $query = $query->where('price', '<=', $filters['price_max']);
+            }
+
+            return response()->json([
+                'data' => $query->get()
+            ]);
+        }
+
         return view('welcome', [
 
-            'products' => Product::paginate(20)
+            'products' => $query->paginate(20),
+            'categories' => ProductCategory::orderBy('name', 'ASC')->get(),
         ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,15 +15,63 @@
 
 <!-- Section-->
 <section class="py-5">
-    <div class="container row-cols-auto text-center text-black">
-        <row>
-            <a href="#">
-                Kategorie
-            </a>
-        </row>
-    </div>
+    <div class="container p-3 my-1 border-3">
+        <h3>FILTRY</h3>
+        <form class="row filtering">
+            <div class="col-12 col-md-4">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <h3 class="h5 card-title">Kategorie</h3>
+                        <div class="row">
+                            <div class="col-6">
+                                @foreach($categories as $category)
+                                    <div class="form-check mb-1">
+                                        <input class="form-check-input" type="checkbox" name="filter[categories][]" id="category-{{$category->id}}" value="{{$category->id}}">
+                                        <label class="form-check-label" for="category-{{$category->id}}">
+                                            {{$category->name}}
+                                        </label>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <h3 class="h5 card-title">Zakres cen</h3>
+                        <!-- Simple slider -->
+                        <div class="input-slider-container">
+                            <div id="input-slider-ecommerce" class="input-slider" data-range-value-min="100" data-range-value-max="500"></div>
+                            <!-- Input slider values -->
+                            <div class="row mt-3 d-none">
+                                <div class="col-6">
+                                    <span id="input-slider-value" class="range-slider-value" data-range-value-low="200"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End of Slider -->
+                        <div class="d-flex mb-3">
+                            <div class="col-md-6 me-2">
+                                <label for="priceRangeMin1">Min</label>
+                                <input type="number" class="form-control" id="price-min" placeholder="50" name="filter[price_min]">
+                            </div>
+                            <div class="col-md-6 text-right">
+                                <label for="priceRangeMax1">Max</label>
+                                <input type="number" class="form-control" id="price-max" placeholder="250" name="filter[price_max]">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="g-grid">
+                <a href="#" class="btn btn-primary" id="filter-button">Zastosuj</a>
+            </div>
+        </form>
+        </div>
     <div class="container px-4 px-lg-5 mt-5">
-        <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+        <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center" id="products-container">
             @foreach($products as $product)
                 <div class="col mb-5">
                     <div class="card h-100">
@@ -58,6 +106,54 @@
 <footer class="py-5 bg-dark">
     <div class="container"><p class="m-0 text-center text-white">| SKLEP INTERNETOWY - AFlo shop |</p></div>
 </footer>
-<!-- Bootstrap core JS-->
+
+@endsection
+@section('javascript')
+    const storagePath = '{{asset('storage')}}/';
+$(function(){
+    $('a#filter-button').click(function() {
+        const form = $('form.filtering').serialize();
+        $.ajax({
+            method:"GET",
+            url: "/",
+            data: form
+        })
+        .done(function (response) {
+            $('div#products-container').empty();
+            $.each(response.data, function (index, product) {
+                let photoHtml;
+                if(product.image_path !==null){
+                    photoHtml = '<img src="' + storagePath + product.image_path + '" class=card-img-top"/>';}
+                else{
+                    photoHtml = '<img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="Grafika produktu" />';
+                }
+                const html = '<div class="col mb-5">'+
+                               ' <div class="card h-100">'+
+                                    <!-- Product image-->
+                                         photoHtml +
+                                    <!-- Product details-->
+                                    '<div class="card-body p-4">'+
+                                        '<div class="text-center">'+
+                                            <!-- Product name-->
+                                            '<h5 class="fw-bolder">'+product.name + '</h5>'+
+                                            <!-- Product price-->
+                                            product.price + 'zł' +
+                                        '</div>'+
+                                    '</div>'+
+                                    <!-- Product actions-->
+                                    '<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">'+
+                                        '<div class="text-center"><a class="btn btn-outline-dark mt-auto" href="/products/'+ product.id +'">Zobacz więcej</a></div>'+
+                                    '</div>'+
+                                '</div>'+
+                            '</div>';
+                    //console.log(photoHtml);
+                $('div#products-container').append(html);
+            });
+        })
+        .fail(function (data) {
+            alert("źle");
+        })
+    });
+});
 
 @endsection
